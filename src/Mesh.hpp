40abe06bca07a8f77853f8cda8fe263a14c58d5f
Paragoumba <paragoumba@gmail.com>
2021-02-08 19:44:16 +0100

    Changed texCoords type from vec3 to vec2

--- src/Mesh.hpp
+++ src/Mesh.hpp
@@ -4,6 +4,7 @@
 #include <vector>
 #include <memory>
 #include <glm/vec3.hpp>
+#include <glm/vec2.hpp>
 
 using std::vector;
 
@@ -15,7 +16,7 @@ namespace engine {
     struct Vertex {
         glm::vec3 position;
         glm::vec3 normal;
-        glm::vec3 texCoords;
+        glm::vec2 texCoords;
     };
 
     struct Texture {
