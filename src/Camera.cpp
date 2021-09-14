ac79563f2fd3cfbe66adb661c1cf3a409af7a7b3
Paragoumba <paragoumba@gmail.com>
2021-09-14 16:09:08 +0200

    Add getViewMatrix

--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,3 +1,16 @@
+#include <glm/ext/matrix_transform.hpp>
 #include "Camera.hpp"
 
 using engine::Camera;
+
+mat4 engine::Camera::getViewMatrix() const {
+
+    mat4 viewMatrix = glm::rotate(mat4(1.0f), glm::radians(rotation.x), vec3(1, 0, 0));
+    viewMatrix = glm::rotate(viewMatrix, glm::radians(rotation.y), vec3(0, 1, 0));
+    viewMatrix = glm::rotate(viewMatrix, glm::radians(rotation.z), vec3(0, 0, 1));
+
+    viewMatrix = glm::translate(viewMatrix, vec3(-position.x, -position.y, -position.z));
+
+    return viewMatrix;
+
+}
--- src/Camera.hpp
+++ src/Camera.hpp
@@ -6,10 +6,13 @@
 #include <interfaces/Rotationable.hpp>
 
 using glm::vec3;
+using glm::mat4;
 
 namespace engine {
 
     class Camera : public Positionable, public Rotationable {
+    public:
+        [[nodiscard]] mat4 getViewMatrix() const;
     };
 }
 
